080a76911ab1ec2b431489c3b99620829224f235
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-04-23 03:48:17 +0530

    LoginActivity

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,19 @@ class MyApp extends StatelessWidget {
 
       debugShowCheckedModeBanner: false,
 
-      home: const Homeactivity(),
+      home: RegisterActivity(),
     );
   }
 }
 
-// class LoginActivity extends StatelessWidget {
-//   const LoginActivity({super.key});
+class RegisterActivity extends StatelessWidget {
+  const RegisterActivity({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold();
-//   }
-// }
-
-// class RegisterActivity extends StatelessWidget {
-//   const RegisterActivity({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold();
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
 
 class Homeactivity extends StatelessWidget {
   const Homeactivity({super.key});
@@ -58,6 +49,64 @@ class Homeactivity extends StatelessWidget {
       backgroundColor: Colors.brown,
       drawer: Drawer(shadowColor: Colors.red),
       // endDrawer: EndDrawerButton(),
+    );
+  }
+}
+
+class LoginActivity extends StatelessWidget {
+  const LoginActivity({super.key});
+
+  void mySnackBar(String message, BuildContext context) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App", style: TextStyle(color: Colors.red)),
+        // titleSpacing: 180,
+        titleTextStyle: TextStyle(
+          color: Colors.blue,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+
+        //centerTitle: true,
+        backgroundColor: Colors.grey,
+        toolbarHeight: 50,
+        toolbarOpacity: 1,
+        elevation: 80,
+
+        actions: [
+          IconButton(
+            onPressed: () {
+              mySnackBar(" i am comments", context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              mySnackBar(" i am settings", context);
+            },
+            icon: Icon(Icons.settings),
+          ),
+          IconButton(
+            onPressed: () {
+              mySnackBar(" i am email", context);
+            },
+            icon: Icon(Icons.email),
+          ),
+          IconButton(
+            onPressed: () {
+              mySnackBar(" i am account", context);
+            },
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
     );
   }
 }
