763332f597c773d43c02c39351a5fa692d71116d
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-04-30 03:25:36 +0530

    json2.dart

---
+++ lib/json2.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+class JsonFormate extends StatefulWidget {
+  const JsonFormate({super.key});
+
+  final List<Map<String, String>> myList = const [
+    {
+      "name": "Badol Majumdar",
+      "Mobile": "700141112706",
+      "Email": "badol@gmail.com",
+      "Password": "123",
+      "Age": "20",
+      "Image": "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d",
+    },
+    {
+      "name": "Shomrat Biswas",
+      "Mobile": "8001412706",
+      "Email": "shomrat@gmail.com",
+      "Password": "1234",
+      "Age": "21",
+      "Image": "https://images.unsplash.com/photo-1516117172878-fd2c41f4a759",
+    },
+    {
+      "name": "Akash Biswas",
+      "Mobile": "9001412706",
+      "Email": "akash@gmail.com",
+      "Password": "12345",
+      "Age": "22",
+      "Image": "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+    },
+    {
+      "name": "Rudra Barmon",
+      "Mobile": "2001412706",
+      "Email": "rudra@gmail.com",
+      "Password": "123456",
+      "Age": "23",
+      "Image": "https://images.unsplash.com/photo-1517841905240-472988babdf9",
+    },
+    {
+      "name": "Chandhon Biswas",
+      "Mobile": "3001412706",
+      "Email": "chandhon@gmail.con",
+      "Password": "1234567",
+      "Age": "23",
+      "Image": "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91",
+    },
+    {
+      "name": "Avi Biswas",
+      "Mobile": "3001412706",
+      "Email": "avi@gmail.com",
+      "Password": "12345678",
+      "Age": "26",
+      "Image": "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde",
+    },
+    {
+      "name": "Bappi Roy",
+      "Mobile": "6001412706",
+      "Email": "bappi@gmail.com",
+      "Password": "28",
+      "Age": "14",
+      "Image": "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
+    },
+    {
+      "name": "Sajib Ray",
+      "Mobile": "726392782",
+      "Email": "sajib@gmail.com",
+      "Password": "226",
+      "Age": "23",
+      "Image": "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+    },
+    {
+      "name": "Piranto Gain",
+      "Mobile": "847454656",
+      "Email": "piranto@gmail.com",
+      "Password": "26",
+      "Age": "12",
+      "Image": "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
+    },
+  ];
+
+  @override
+  State<JsonFormate> createState() => _JsonFormateState();
+}
+
+class _JsonFormateState extends State<JsonFormate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("JSON Format Example"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: widget.myList.length,
+        itemBuilder: (context, index) {
+          var item = widget.myList[index];
+
+          return GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(item["name"]!)));
+            },
+            child: Container(
+              margin: const EdgeInsets.all(12),
+              height: 220,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      item["Image"]!,
+                      width: double.infinity,
+                      height: 220,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.8),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    bottom: 15,
+                    left: 15,
+                    right: 15,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item["name"]!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          item["Email"]!,
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Positioned(
+                    top: 15,
+                    right: 15,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Age ${item["Age"]!}",
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    top: 15,
+                    left: 15,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        item["Mobile"]!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
